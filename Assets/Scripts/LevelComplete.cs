8fe96432b08a26d2580999e04c94bbf79be32eb3
NACKADEMIN\jugu2101 <Julia.gulliksson@yh.nackademin.se>
2021-10-19 14:03:11 +0200

    stop loading next scene for now

---
+++ Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        //TODO: Just restart the level, no additional levels for now
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
