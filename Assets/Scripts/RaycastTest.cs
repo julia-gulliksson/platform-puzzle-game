3355965cdbd62520a317a386ba15d4112f0755fc
julia-gulliksson <Julia.Gulliksson@yh.nackademin.se>
2021-10-20 14:23:11 +0200

    make object pickup work

--- Assets/Scripts/RaycastTest.cs
+++ Assets/Scripts/RaycastTest.cs
@@ -7,6 +7,7 @@ public class RaycastTest : MonoBehaviour
     GameObject heldObject;
     [SerializeField] float pickUpRange = 5;
     [SerializeField] Transform player;
+    bool shiftKeyIsPressed = false;
     void Start()
     {
 
@@ -17,16 +18,32 @@ public class RaycastTest : MonoBehaviour
     {
         //Debug.Log(transform.position + "position");
         //Debug.Log("right" + transform.TransformDirection(Vector3.right));
-        Vector3 forward = transform.TransformDirection(0, -1, 0) * 10;
+        Vector3 forward = transform.TransformDirection(0, -1, 0);
         Debug.DrawRay(transform.position, forward, Color.green);
-        if (heldObject == null)
+        if (Input.GetButton("Fire3") && heldObject == null)
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.TransformDirection(0, -1, 0), out hit, pickUpRange))
             {
-                Debug.Log(hit.transform.gameObject.name);
-                //PickUpObject(hit.transform.gameObject);
+                //Debug.Log(hit.transform.gameObject.name);
+                PickUpObject(hit.transform.gameObject);
             }
+        }
+    }
+
+    void PickUpObject(GameObject objectToPickUp)
+    {
+        if (objectToPickUp.layer == 9)
+        {
+            // Ignore collision with hands + player and object, to prevent kinematic collider to cause player to fly
+            Physics.IgnoreCollision(objectToPickUp.GetComponent<Collider>(), transform.GetComponent<Collider>());
+            Physics.IgnoreCollision(objectToPickUp.GetComponent<Collider>(), transform.parent.GetComponent<Collider>());
+
+            Rigidbody objectRb;
+            objectRb = objectToPickUp.GetComponent<Rigidbody>();
+            objectToPickUp.transform.parent = transform.parent;
+            objectRb.isKinematic = true;
+            objectToPickUp.transform.position = new Vector3(objectToPickUp.transform.position.x, transform.parent.position.y - 0.2f, 0);
         }
     }
 }
