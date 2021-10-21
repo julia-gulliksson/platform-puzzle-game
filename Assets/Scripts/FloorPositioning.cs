7f4308b1cb5a80ebb8809036c30a9776dbc34775
julia-gulliksson <Julia.Gulliksson@yh.nackademin.se>
2021-10-21 15:40:34 +0200

    change animation, delete some prefabs

---
+++ Assets/Scripts/FloorPositioning.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorPositioning : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = transform.GetChild(0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
