64d14b9ee02036eb4a5a34715315fac5d27a1961
NACKADEMIN\jugu2101 <Julia.gulliksson@yh.nackademin.se>
2021-10-26 11:43:00 +0200

    refine platform trigger detection

--- Assets/MyAssets/Scripts/PlatformTrigger.cs
+++ Assets/MyAssets/Scripts/PlatformTrigger.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlatformTrigger : MonoBehaviour
 {
     [SerializeField] Animator animationController;
+    List<GameObject> collidingObjects = new List<GameObject>();
 
     private void OnTriggerEnter(Collider other)
     {
         animationController.SetBool("trigger", true);
+        collidingObjects.Add(other.gameObject);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        animationController.SetBool("trigger", false);
+        collidingObjects.Remove(other.gameObject);
+        if (collidingObjects.Count == 0)
+        {
+            //Make sure no other objects are colliding, for example the cube has exited but the player is still colliding
+            animationController.SetBool("trigger", false);
+        }
     }
-
 }
