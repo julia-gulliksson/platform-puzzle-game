2abbaff5df340e80c33b1c93b1600cfa3680ae21
NACKADEMIN\jugu2101 <Julia.gulliksson@yh.nackademin.se>
2021-10-26 12:47:05 +0200

    decrease platformtrigger collider

--- Assets/MyAssets/Scripts/PlayerCollision.cs
+++ Assets/MyAssets/Scripts/PlayerCollision.cs
@@ -31,7 +31,7 @@ public class PlayerCollision : MonoBehaviour
         coins++;
     }
 
-    public void RemoveSuperJump()
+    public void DecreaseSuperJump()
     {
         superJumpsRemaining--;
     }
